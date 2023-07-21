537cd4bc873fca3d6b38afe02d836099a986769a
timwalkercs <timothywalker2000@gmail.com>
2023-07-20 20:06:31 -0400

    Converted Home.js from class component to functional.

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -3,14 +3,17 @@ import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
 
 export class Layout extends Component {
-  static displayName = Layout.name;
 
+    //Layout Component, arranges the layout of the webpage
+        //The NavMenu is placed across the top and another component can be passed into the container to be displayed dynamically
+
+  static displayName = Layout.name;
   render() {
     return (
-      <div>
+        <div>
         <NavMenu />
         <Container>
-          {this.props.children}
+          {this.props.children  /* The component that will be passed into the container */}
         </Container>
       </div>
     );
